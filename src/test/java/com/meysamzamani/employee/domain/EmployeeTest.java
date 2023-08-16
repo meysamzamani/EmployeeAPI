0a62812854f5ae82f6f27c1989cc11aab53e1449
meysamzamani <meysamzamani@gmail.com>
2023-08-16 19:09:19 +0200

    fix: add unit test for check two same Employee field by field

--- src/test/java/com/meysamzamani/employee/domain/EmployeeTest.java
+++ src/test/java/com/meysamzamani/employee/domain/EmployeeTest.java
@@ -1,5 +1,6 @@
 package com.meysamzamani.employee.domain;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -7,6 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 public class EmployeeTest {
@@ -14,12 +16,12 @@ public class EmployeeTest {
     private Employee employee;
 
     @BeforeEach
-    public void setup() {
+    void setup() {
         employee = new Employee();
     }
 
     @Test
-    public void givenProperties_whenSet_thenShouldEquals() {
+    void givenProperties_whenSet_thenShouldEquals() {
 
         UUID id = UUID.randomUUID();
         String email = "test@test.com";
@@ -42,7 +44,7 @@ public class EmployeeTest {
     }
 
     @Test
-    public void givenProperties_whenToString_thenShouldEqualExpected() {
+    void givenProperties_whenToString_thenShouldEqualExpected() {
 
         String email = "test@test.com";
         String firstName = "Meysam";
@@ -64,5 +66,15 @@ public class EmployeeTest {
                 "}";
 
         assertEquals(expectedString, employee.toString());
+    }
+
+    @Test
+    void givenEmployees_whenIsSame_thenShouldEquals() {
+        List<String> hobbies = Arrays.asList("soccer", "music");
+
+        Employee employee1 = new Employee("test1@example.com", LocalDate.of(1987, 5, 20), "Meysam", "Zamani", hobbies);
+        Employee employee2 = new Employee("test1@example.com", LocalDate.of(1987, 5, 20), "Meysam", "Zamani", hobbies);
+
+        assertThat(employee1).usingRecursiveComparison().isEqualTo(employee2);
     }
 }
